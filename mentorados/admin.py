9af04c62b339420077417e809942675770f5845a
JBVJoaoV <joaopedrodesousavieira@gmail.com>
2025-04-06 18:16:23 -0300

    finalização do projeto

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Navigators, Mentorados, DisponibilidadeDeHorarios
+from .models import Navigators, Mentorados, DisponibilidadeDeHorarios, Reuniao
 
 # Register your models here.
 admin.site.register(Navigators)
 admin.site.register(Mentorados)
 admin.site.register(DisponibilidadeDeHorarios)
+admin.site.register(Reuniao)
